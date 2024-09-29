d221399730faa9a7b3460b41d1910972b1d4075e
Ihor <igorskor0912@gmail.com>
2024-09-29 15:27:38 +0300

    Add a divison operation and patches

--- calculator.cpp
+++ calculator.cpp
@@ -1,4 +1,5 @@
 #include "calculator.h"
+#include <stdexcept> // To use runtime_error
 
 int Calculator::Add (double a, double b)
 {
@@ -13,4 +14,11 @@ int Calculator::Sub (double a, double b)
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+double Calculator::Div(double a, double b)
+{
+    if(b == 0){
+       throw runtime_error("Math error: Attempted to divide by Zero\n");
+    }
+    return a / b;
 }
